185b433ae8ce19d47d67df35040274ddca0ce88c
Dale Salter <djsalter93@hotmail.com>
2014-02-03 17:18:40 +1100

    fixed some g++ warnings

--- main.cpp
+++ main.cpp
@@ -5,13 +5,16 @@
 #include "my_string_library.h"
 
 void scope(){
+    // Fixes: conversion from string literal to 'char *' is deprecated [-Wdeprecated-writable-strings]
+    char num_string[] = "0123456789";
+    char* ptr_num_string = num_string;
 
-    MyString more_nums("0123456789");
+    MyString more_nums(ptr_num_string);
     for (char* i = (char*)more_nums.first(); i != (char*)more_nums.last(); ++i){
         std::cout << (void*)i << ':' << *i << std::endl;
     }
 
-    MyString* nums = new MyString("123456789");
+    MyString* nums = new MyString(ptr_num_string);
     for (char* i = (char*)nums->first(); i != (char*)nums->last(); ++i){
         std::cout << (void*)i << ':' << *i << std::endl;
     }
@@ -19,8 +22,13 @@ void scope(){
     delete nums;
 }
 
+
 void scope2(){
-    MyString more_nums[2] = { MyString("blah"), MyString(" other blah") };
+    // Fixes: conversion from string literal to 'char *' is deprecated [-Wdeprecated-writable-strings]
+    char a_cstring[][5] = {"blah", " bob"};
+    char* ptr_a_cstring[] = {a_cstring[0], a_cstring[1]};
+
+    MyString more_nums[2] = { MyString(ptr_a_cstring[0]), MyString(ptr_a_cstring[1]) };
 
     std::cout << more_nums[0] << more_nums[1] << std::endl;
 }
